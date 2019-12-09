c13ddf5b0b2a56cc61e369cfe53d4a9bb9867186
KTY923 <963941911@qq.com>
2019-12-10 00:25:43 +0800

    ex1.c

---
+++ lab5/ex1.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+#include<stdio.h>
+void reverse(char s[])
+{	if(s[0]!='\0')
+	{	reverse(s+1);
+		printf("%c",*s);
+	}
+}
+main()
+{
+	char s[200]={'\0'};
+	printf("ÇëÊäÈëÒ»¸ö×Ö·û´®£º\n");
+	gets(s);
+	reverse(s);
+	printf("\n");
+
+}
+	
