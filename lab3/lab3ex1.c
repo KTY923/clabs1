20776196d280551960570db7ed63e088009b3828
KTY923 <963941911@qq.com>
2019-12-27 17:19:03 +0800

    lab3ex1.c

--- lab3/lab3ex1.c
+++ lab3/lab3ex1.c
@@ -1,43 +1,28 @@
 #include <stdio.h>
-#define MAXLINE 1000
-
-int getline(char line[], int max);
-int strindex(char source[], char searchfor[]);
-
-char pattern[]="ould";
-main()
+int main()
 {
-	char line[MAXLINE];
-	int found=0;
-
-	while (getline(line, MAXLINE)>0){
-		if (strindex(line, pattern)>=0){
-			printf("%s", line);
-			found++;
-		}
-	}
-	return found;
-}
-int getline(char s[], int lim)
-{
-	int c, i;
-	i=0;
-	while (--lim>0&&(c=getchar())!=EOF&&c!='\n')
-		s[i++]=c;
-	if (c=='\n')
-		s[i++]=c;
-	s[i]='\0';
-	return i;
-}
-int strindex(char s[], char t[])
-{
-	int i, j, k;
-
-	for (i=0; s[i]!='\0'; i++){
-		for (j=i, k=0; t[k]!='\0'&& s[j]==t[k]; j++, k++)
-			;
-		if (k>0 && t[k]=='\0')
-        return i;
+    char s[1000];
+    char t[1000];
+    int m = 0;
+    int n =0;
+    while ((s[m] = getchar()) != ' ')
+    {
+        m++;
     }
-		return -1;
+    while (((t[n] = getchar()) != EOF) && (t[n] != '\n') && s[m] == ' ')
+    {
+        n++;
+    }
+    int i, j, k;
+    for (i = m - 1; i > 0; i--)
+    {
+        for (j = i, k = n - 1; s[j] == t[k]; j--, k--)
+            ;
+        if (k < 0)
+        {
+            printf("%d\n", j + 1);
+            return i;
+        }
+    }
+    printf("wrong\n");
 }
