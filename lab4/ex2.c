8c0a59201fcb41a7dbfe5d4641f1be5af3d29aec
KTY923 <963941911@qq.com>
2019-12-27 17:08:41 +0800

    ex2.c

--- lab4/ex2.c
+++ lab4/ex2.c
@@ -1,204 +1,160 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
-#define BUFSIZE 100
+#define MAXOP 100
 #define NUMBER '0'
+int getop(char[]);
+void push(double);
+double pop(void); 
+double top(void);
+extern int sp;
+int main()
+{ 
+	printf("t means print the top factot\n");
+	printf("e means exchange the topest two factors\n");
+	printf("c means copy the top factor\n");
+	printf("d means delete the val\n");
+    int type;
+    double op2, op0, copy; 
+    char s[MAXOP];
+    while ((type = getop(s)) != EOF)
+    {
+        switch (type)
+        {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0) 
+                push(pop() / op2);
+            else
+                printf("errer:zero divisor\n");
+            break;
+        case '%': 
+            op2 = pop();
+            if (op2 != 0)
+                push(((int)pop()) % ((int)op2));
+            else
+                printf("errer:zero divisor\n");
+            break;
+        case 't': 
+            printf("%.8g\n", top());
+            break;
+        case 'e': 
+            op2 = pop();
+            op0 = pop(); 
+            push(op2);
+            push(op0);
+            break;
+        case 'c': 
+            copy = top();
+            push(copy);
+            break;
+        case 'd': 
+            sp = 0;
+            break; 
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+        default:
+            printf("error:unknow command %s\n", s);
+            break;
+        }
+    }
+    return 0;
+}
 #define MAXVAL 100
-
-char buf[BUFSIZE];
-int bufp=0;
 int sp = 0;
 double val[MAXVAL];
-
-
-void ungetch(int);
-int getch(void);
-int getop(char []);
-void push(double);
-double pop(void);
-void clear(void) ;
-void top(void);
-double copy(void);
-
-main()
-{
-    int type;
-    double op2;
-    char var[MAXVAL];
-	int sign=1;
-
-    while ((type = getop(val)) != EOF)
-	{
-        switch (type)
-		{	
-			//case '':
-				
-			case NUMBER:
-				push(atof(val));
-				break;
-			case '+':
-				push(pop() + pop());
-				break;
-			case '*':
-				push(pop() * pop());
-				break;
-			case '-':
-				op2 = pop();
-				push(pop() - op2);
-				break;
-			case '$':
-				op2 = pop();
-				push(pop() - op2);
-				printf("\t%.8g\n", pop());
-				break;
-			case '/':
-				op2 = pop();
-				if (op2 != 0.0)
-					push(pop() / op2);
-				else 
-					printf("error:zero divisor\n");
-				break;
-			case '%':
-				op2=pop();
-				if(op2 != 0.0)
-				push((int)pop() % (int)op2);
-				else
-				printf("error: zero divisro \n");
-				break;
-			case '\n':
-				printf("\t%.8g\n", pop());
-				break;
-			default:
-				printf("error:unknown command %s\n", val);
-		}
-	}
-return 0;
-}
-
-
 void push(double f)
 {
-    if(sp < MAXVAL)
+    if (sp < MAXVAL)
         val[sp++] = f;
     else
-        printf("error:stack full, can't push %g\n", f);
+        printf("error:stack full,can not push %g\n", f);
 }
-
 double pop(void)
 {
-    if(sp > 0)
+    if (sp > 0)
         return val[--sp];
-    else{
+    else
+    {
         printf("error:stack empty\n");
         return 0.0;
     }
 }
-
-void top(void)
-{	
-	if (sp>0)
-	{	sp=sp-1;
-		printf("top of stack : %f\n",val[sp]);
-		sp=sp+1;
-	}
-	else
-		printf("error: stack empty\n");
-	
-
+double top(void)
+{
+    double result;
+    if (sp > 0)
+    {
+        result = val[--sp];
+        ++sp;
+        return result;
+    }
+    else
+    {
+        printf("error:stack empty\n");
+        return 0.0;
+    }
 }
-
-double copy(void)
-{	double copy;
-	if (sp>0)
-	{	sp=sp-1;
-		copy=val[sp];
-		sp=sp+1;
-		return copy;
-	}
-	else
-	{
-		printf("error: stack empty\n");
-		return 0.0;
-	
-	}
-
+#include <ctype.h>
+int getch(void);
+void ungetch(int);
+int getop(char s[])
+{
+    int i, c;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    i = 0;
+    if (!isdigit(c) && c != '.' && c != '-') /*dealing with negative numbers*/
+        return c;
+    if (c == '-')
+    {
+        if (isdigit(c = getch()))
+            s[++i] = c;
+        else
+        {
+            if (c != EOF)
+            {
+                ungetch(c);
+            }
+            return '-';
+        }
+    }
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
 }
-
-void exchange(void) 
-{	
-	if (sp>1)
-	{	double a;
-		int i=sp;
-		a=val[i-1];
-		val[i-1]=val[i-2];
-		val[i-2]=a;
-	}
-	else
-		printf("error: value not enough \n");
-	
-}
-
-void clear(void) 
-{	
-	if (sp>0)
-		for(sp=sp-1;sp>=0;sp--)
-			val[sp]='\0';
-	
-	else
-		printf("error: stack empty\n");
-}
-
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
 int getch(void)
-{	return (bufp>0) ? buf[--bufp]:getchar();
-
-
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
 }
 void ungetch(int c)
-{	if(bufp >=BUFSIZE)
-		printf("ungetch: too many characters\n");
-	else
-		buf[bufp++]=c;
-
-}
-
-int getop(char s[])
-{	int i,c;
-	int sign=1;
-	while ((s[0]=c=getch())==' '||c=='\t')
-		;
-	s[1]='\0';
-	if(c=='-')
-	{	i=0;
-		if(isdigit(c=getch()))
-		{	s[++i]=c;	
-			while(isdigit(s[++i]=c=getch()))
-			;
-			s[i]='\0';
-			if(c!= EOF)
-				ungetch;
-			return NUMBER;
-		}
-		else
-			if(c==' ')
-				return '-';
-			else
-				return '$';
-
-	
-	}
-	if(!isdigit(c)&&c!='.')
-	{	//if
-		return c;
-	}
-	i=0;
-	if(isdigit(c))
-		while(isdigit(s[++i]=c=getch()))
-			;
-	s[i]='\0';
-	if(c!= EOF)
-		ungetch;
-	return NUMBER;
-
-
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch:too many characters\n");
+    else
+        buf[bufp++] = c;
 }
